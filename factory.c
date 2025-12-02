11931b1e2737eb9b1dbc331407a1335330456d7e
kamirra <mirraka@dukes.jmu.edu>
2025-12-01 22:55:16 -0500

    completed factory.c and procurement.c

--- factory.c
+++ factory.c
@@ -1,12 +1,13 @@
 //---------------------------------------------------------------------
-// Assignment : PA-03 UDP Single-Threaded Server
-// Date       : 11/21/2025
+// Assignment : PA-04 Threads - UDP
+// Date       : 12/1/2025
 // Author     : Kyle Mirra      Akwasi Okyere
 // File Name  : factory.c
 //---------------------------------------------------------------------
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -28,12 +29,26 @@
 
 typedef struct sockaddr SA ;
 
+typedef struct {
+    int facID;
+    int capacity;
+    int duration;
+} factoryArgs;
+
+typedef struct {
+    int facID;
+    int totalParts;
+    int iterations;
+} factoryResults;
+
 int minimum( int a , int b)
 {
     return ( a <= b ? a : b ) ; 
 }
 
-void subFactory( int factoryID , int myCapacity , int myDuration ) ;
+void subFactory( int factoryID , int myCapacity , int myDuration, factoryResults *res ) ;
+
+void *subFactoryThread(void *arg);
 
 void factLog( char *str )
 {
@@ -57,6 +72,7 @@ struct sockaddr_in
              srvrSkt,       /* the address of this server   */
              clntSkt;       /* remote client's socket       */
 
+char  *myName = "Kyle Mirra and Akwasi Okyere" ;
 //------------------------------------------------------------
 //  Handle Ctrl-C or KILL 
 //------------------------------------------------------------
@@ -89,18 +105,12 @@ int main( int argc , char *argv[] )
 {
     sigactionWrapper(SIGTERM, goodbye);
     sigactionWrapper(SIGINT, goodbye);
-
-    char  *myName = "Kyle Mirra and Akwasi Okyere" ; 
+ 
     unsigned short port = 50015 ;      /* service port number  */
     int    N = 1 ;                     /* Num threads serving the client */
     socklen_t     addrLen;          /* from-address length          */
 
-    printf("\nThis is the FACTORY server developed by %s\n\n" , myName ) ;
-    char myUserName[30] ;
-    getlogin_r ( myUserName , 30 ) ;
-    time_t  now;
-    time( &now ) ;
-    fprintf( stdout , "Logged in as user '%s' on %s\n\n" , myUserName ,  ctime( &now)  ) ;
+    printf("\nThis is the FACTORY server (by %s )\n\n" , myName ) ;
     fflush( stdout ) ;
 
 	switch (argc) 
@@ -122,6 +132,9 @@ int main( int argc , char *argv[] )
         printf( "FACTORY Usage: %s [numThreads] [port]\n" , argv[0] );
         exit( 1 ) ;
     }
+
+    printf("I will attempt to accept orders at port %hu with %d sub-factories\n", port, N);
+    fflush(stdout);
 
     // Create the socket
     sd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -150,15 +163,22 @@ int main( int argc , char *argv[] )
     int forever = 1;
     while ( forever )
     {
+        // Variables to measure the time
+        struct timeval startTime, endTime;
+        long elapsedMS;
+
+        // Array to store the factory thread result structs
+        factoryResults results[N]; 
+
         addrLen = sizeof(clntSkt);
-        printf( "\nFACTORY server waiting for Order Requests\n" ) ; 
+        printf( "\nFACTORY server ( by %s ) waiting for Order Requests\n", myName ) ; 
 
         // Wait to receive request message
         msgBuf rcvMsg;
         if (recvfrom(sd, (void *) &rcvMsg, sizeof(rcvMsg), 0, (SA *) &clntSkt, &addrLen) < 0) {
             err_sys("Error receiving the order request from the client");
         }
-        printf("\n\nFACTORY server received: " ) ;
+        printf("\n\nFACTORY server (by %s ) received: ", myName ) ;
         printMsg( & rcvMsg );  puts("");
 
         char clientIP[IPSTRLEN];
@@ -174,22 +194,76 @@ int main( int argc , char *argv[] )
 
         // Create the confirmation message
         msgBuf cnfMsg;
-        cnfMsg.numFac = htonl(1);
+        cnfMsg.numFac = htonl(N);
         cnfMsg.purpose = htonl(ORDR_CONFIRM);
 
         // Send the confirmation message
         if (sendto(sd, (void *)&cnfMsg, sizeof(cnfMsg), 0, (SA * ) &clntSkt, sizeof(clntSkt)) < 0) {
             err_sys("Error sending the order confirmation message");
         }
-        printf("\n\nFACTORY sent this Order Confirmation to the client " );
+        printf("\n\nFACTORY ( by %s ) sent this Order Confirmation to the client ", myName );
         printMsg(  & cnfMsg );  puts("");
         
-        subFactory( 1 , 50 , 350 ) ;  // Single factory, ID=1 , capacity=50, duration=350 ms
+        gettimeofday(&startTime, NULL); // Get start time
+     
+        pthread_t tids[N]; // Array to store each thread's id
+        srandom((unsigned) time(NULL)); // Create random number generator seed 
+        
+        //Create N threads
+        for (int i = 0; i < N; i++) {
+            // Set the argument structure for the thread routine
+            factoryArgs *args = malloc(sizeof(factoryArgs));
+            args->facID = i + 1;
+            args->capacity  = (random() % 41) + 10;     // random number from 10–50
+            args->duration  = (random() % 701) + 500;   // random number from 500–1200
+
+            pthread_create(&tids[i], NULL, subFactoryThread, args);
+            printf("Created Factory Thread #%-3d with capacity = %-4d parts and duration = %-5d mSecs\n",
+                args->facID, args->capacity, args->duration);
+        }
+
+        factoryResults *threadRes;
+        // Wait for all factories to finish
+        for (int i = 0; i < N; i++) {
+            pthread_join(tids[i], (void **)&threadRes);
+
+            // Store the result of the thread routine in the result array
+            results[i] = *threadRes;
+            free(threadRes);
+        }
+
+        // get ending time and calculate total time
+        gettimeofday(&endTime, NULL); 
+        elapsedMS = (endTime.tv_sec - startTime.tv_sec) * 1000L +
+            (endTime.tv_usec - startTime.tv_usec) / 1000L;
+
+        printf("\n****** FACTORY Server (by %s ) Summary Report ******\n", myName);
+        printf("\tSub-Factory\tParts Made\tIterations\n");
+
+        int totalMade;
+        for (int i = 0; i < N; i++) {
+            printf("\t\t%-3d\t\t%-5d\t\t%-3d\n", results[i].facID, results[i].totalParts, results[i].iterations);
+            totalMade += results[i].totalParts;
+        }
+        printf("======================================================\n");
+        printf("Grand total parts made  =   %-5d vs order size %-5d\n", totalMade, orderSize);
+        printf("Order-to-Completion time =  %ld milliSeconds\n", elapsedMS);
     }
     return 0 ;
 }
 
-void subFactory( int factoryID , int myCapacity , int myDuration )
+// Thread routine
+void *subFactoryThread(void *arg) {
+    factoryArgs *args = (factoryArgs *)arg;
+    factoryResults *res = malloc(sizeof(factoryResults));
+
+    // Each thread calls the subFactory() method
+    subFactory(args->facID, args->capacity, args->duration, res);
+    free(args);
+    return res;
+}
+
+void subFactory( int factoryID , int myCapacity , int myDuration, factoryResults *res)
 {
     char    strBuff[ MAXSTR ] ;   // snprint buffer
     int     partsImade = 0 , myIterations = 0 ;
@@ -214,7 +288,7 @@ void subFactory( int factoryID , int myCapacity , int myDuration )
         partsImade += partsToMake;
         myIterations++;
 
-        printf("Factory #%3d: Going to make %5d parts in %4d mSec\n", factoryID, partsToMake, myDuration);
+        printf("Factory (%s) #%3d: Going to make %5d parts in %4d mSec\n", myName, factoryID, partsToMake, myDuration);
 
         // Send a Production Message to Supervisor
         msg.facID = htonl(factoryID);
@@ -228,9 +302,13 @@ void subFactory( int factoryID , int myCapacity , int myDuration )
         }
     }
 
+    res->facID = factoryID;
+    res->iterations = myIterations;
+    res->totalParts = partsImade;
+
     // Send a Completion Message to Supervisor
     msgBuf cmpMsg;
-    cmpMsg.facID = htonl(1);
+    cmpMsg.facID = htonl(factoryID);
     cmpMsg.purpose = htonl(COMPLETION_MSG);
 
     if (sendto(sd, (void *) &cmpMsg, sizeof(cmpMsg), 0, (SA *) &clntSkt, sizeof(clntSkt)) < 0) {
--- procurement.c
+++ procurement.c
@@ -1,12 +1,13 @@
 //---------------------------------------------------------------------
-// Assignment : PA-03 UDP Single-Threaded Server
-// Date       : 11/21/2025
+// Assignment : PA-04 Threads - UDP
+// Date       : 12/1/2025
 // Author     : Kyle Mirra      Akwasi Okyere
 // File Name  : procurement.c
 //---------------------------------------------------------------------
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -34,16 +35,13 @@ int main( int argc , char *argv[] )
             iters[ MAXFACTORIES+1 ] = {0} ,  // num Iterations completed by each Factory
             partsMade[ MAXFACTORIES+1 ] = {0} , totalItems = 0;
 
+    struct timeval startTime, endTime; // starting and ending time
+    long elapsedMS; // total time taken
+
     socklen_t addrLen;
 
     char  *myName = "Kyle Mirra and Akwasi Okyere" ; 
-    printf("\nPROCUREMENT: Started. Developed by %s\n\n" , myName );    
-
-    char myUserName[30] ;
-    getlogin_r ( myUserName , 30 ) ;
-    time_t  now;
-    time( &now ) ;
-    fprintf( stdout , "Logged in as user '%s' on %s\n\n" , myUserName ,  ctime( &now)  ) ;
+    printf("\nThis is procurement. ( by %s )\n\n", myName);
     fflush( stdout ) ;
     
     if ( argc < 4 )
@@ -94,9 +92,10 @@ int main( int argc , char *argv[] )
         err_sys("Error receiving order confirmation message");
     }
 
-    printf("PROCUREMENT received this from the FACTORY server: "  );
+    printf("PROCUREMENT ( by %s ) received this from the FACTORY server: ", myName);
     printMsg( & msg2 );  puts("\n");
 
+    gettimeofday(&startTime, NULL); 
     numFactories = ntohl(msg2.numFac);
     activeFactories = numFactories;
 
@@ -118,35 +117,42 @@ int main( int argc , char *argv[] )
         if (purpose == PRODUCTION_MSG) {
         iters[facID]++;
         partsMade[facID] += msgPartsMade;
-        printf("PROCUREMENT: Factory #%3d produced %5d parts in %5d milliSecs\n", facID, msgPartsMade, duration);
+        printf("PROCUREMENT ( by %s ): Factory #%-3d produced %-5d parts in %-5d milliSecs\n", myName, facID, msgPartsMade, duration);
         } 
         else if (purpose == COMPLETION_MSG) {
             activeFactories--;
-            printf("PROCUREMENT:rn were not  Factory #%d         COMPLETED its task\n", facID);
+            printf("PROCUREMENT ( by %s ): Factory #%-3d         COMPLETED its task\n", myName, facID);
         }
         else if (purpose == PROTOCOL_ERR){
-            printf("PROCUREMENT: Received invalid msg ");
+            printf("PROCUREMENT ( by %s ): Received invalid msg ", myName);
             printMsg(&updtMsg); puts("");
             close(sd);
             exit(1);
         } else {
-            printf("PROCUREMENT: Received an invalid message\n");
+            printf("PROCUREMENT ( by %s ): Received an invalid message\n", myName);
             close(sd);
             exit(1);
         }
     } 
+    // Get ending time and calculate total time
+    gettimeofday(&endTime, NULL);
+    elapsedMS = (endTime.tv_sec - startTime.tv_sec) * 1000L +
+            (endTime.tv_usec - startTime.tv_usec) / 1000L;
 
     // Print the summary report
     totalItems  = 0 ;
-    printf("\n\n****** PROCUREMENT Summary Report ******\n");
+    printf("\n\n****** PROCUREMENT ( by %s ) Summary Report ******\n", myName);
+    printf("\tSub-Factory\tParts Made\tIterations\n");
+
     for (int i = 1; i <= numFactories; i++) {
-        printf("Factory #%3d made a total of %5d parts in %3d iterations\n", i, partsMade[i], iters[i]);
+        printf("\t\t%-3d\t\t%-5d\t\t%-3d\n", i, partsMade[i], iters[i]);
         totalItems += partsMade[i];
     }
 
-    printf("==============================\n") ;
+    printf("=========================================================\n") ;
 
     printf("Grand total parts made = %5d vs order size of %5d\n", totalItems, orderSize);
+    printf("Order-to-Completion time = %ld milliSeconds\n", elapsedMS);
 
     printf( "\n>>> PROCUREMENT Terminated\n");
 
